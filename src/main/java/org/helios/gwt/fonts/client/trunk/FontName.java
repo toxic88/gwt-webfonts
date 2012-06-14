bb56c5e293a77983621d761888a5d12784eda407
helios.alonso@gmail.com <helios.alonso@gmail.com@821e44f4-d933-52fc-9b11-a56ad7906792>
2012-06-14 10:24:42 +0000

    Initial import

---
+++ src/main/java/org/helios/gwt/fonts/client/trunk/FontName.java
@@ -0,0 +1,10 @@
+package org.helios.gwt.fonts.client;
+
+/**
+ * use this annotation to change the font-name used in the rendered page
+ * for the {@link FontResource} it applies to.
+ */
+public @interface FontName 
+{
+	String value();
+}
